1a65a8dc7fa23d90f6b9dbc34fc7135d90c782ce
alimomtaznoor <noorali.momtaz98@gmail.com>
2025-04-23 17:28:20 +0530

    error resoloved

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
   return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
 }
 
-export async function POST({ request: Request }) {
+export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
 
   try {
