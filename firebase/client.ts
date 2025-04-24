a5b4a6c91f0be7ba17e2a3613851a2264ee94f11
alimomtaznoor <noorali.momtaz98@gmail.com>
2025-04-24 11:28:53 +0530

    firebase issue resolved

--- firebase/client.ts
+++ firebase/client.ts
@@ -19,8 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
-
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
