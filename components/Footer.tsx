69e8bc126edc57d6227db5586ce930d7031b8594
alimomtaznoor <noorali.momtaz98@gmail.com>
2025-04-27 12:07:02 +0530

    footer updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className='flex items-center justify-between mt-10'>
           <div>
               <Link href="/terms">
-                  <h1 className='text-sm text-[#3c3c3c]'>Privacy Policy</h1>
+                  <h1 className='text-sm text-[#6d6d6d]'>Privacy Policy</h1>
               </Link>
           </div>
           <h1 className='text-sm text-[#656464]'>Made by <Link href="https://github.com/alimomtaznoor">@alimomtaznoor</Link></h1>
