d351984c2ca205fdad0818de36998a1c51351152
alimomtaznoor <noorali.momtaz98@gmail.com>
2025-04-27 12:18:57 +0530

    added socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,28 @@
 import Link from 'next/link'
 import React from 'react'
+import { SiBuymeacoffee } from "react-icons/si";
 
 const Footer = () => {
   return (
-      <div className='flex items-center justify-between mt-10'>
-          <div>
-              <Link href="/terms">
-                  <h1 className='text-sm text-[#6d6d6d]'>Privacy Policy</h1>
-              </Link>
-          </div>
-          <h1 className='text-sm text-[#656464]'>Made by <Link href="https://github.com/alimomtaznoor">@alimomtaznoor</Link></h1>
+    <div className="flex items-center justify-between mt-10">
+      <div>
+        <Link href="/terms">
+          <h1 className="text-sm text-[#6d6d6d]">Privacy Policy</h1>
+        </Link>
+      </div>
+
+      <h1 className="text-sm text-[#656464]">
+        Made by{" "}
+        <Link href="https://github.com/alimomtaznoor">@alimomtaznoor</Link>
+      </h1>
+
+      <div>
+        <Link href="https://www.buymeacoffee.com/alimomtaznoor">
+          <SiBuymeacoffee color="#524c8b" />
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
