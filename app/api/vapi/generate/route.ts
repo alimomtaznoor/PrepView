79510c9a45c5e9b44ef569cc481ec4ce3db52c29
alimomtaznoor <noorali.momtaz98@gmail.com>
2025-04-29 11:46:41 +0530

    added new question to the voice generator

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -5,16 +5,16 @@ import { db } from "@/firebase/admin";
 import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
-  const { type, role, level, techstack, amount, userid } = await request.json();
+  const { intro, role, level, techstack, amount, userid } = await request.json();
 
   try {
     const { text: questions } = await generateText({
       model: google("gemini-2.0-flash-001"),
       prompt: `Prepare questions for a job interview.
+        The technical background or introduction of the candidate is ${intro}.
         The job role is ${role}.
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
-        The focus between behavioural and technical questions should lean towards: ${type}.
         The amount of questions required is: ${amount}.
         Please return only the questions, without any additional text.
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
@@ -26,8 +26,8 @@ export async function POST(request: Request) {
     });
 
     const interview = {
+      intro: intro,
       role: role,
-      type: type,
       level: level,
       techstack: techstack.split(","),
       questions: JSON.parse(questions),
